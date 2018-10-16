71056c5d617c612261a12fc17544d35edab2cc50
Nelson Elhage <nelhage@nelhage.com>
2018-10-16 03:21:25 +0000

    add a benchmark

---
+++ cxx/rubik_bench.cc
@@ -0,0 +1,40 @@
+#include <chrono>
+#include <iostream>
+
+#include "rubik.h"
+
+using namespace rubik;
+using namespace std;
+
+constexpr uint64_t N_ITERATIONS = 1 << 20;
+
+template<typename T>
+void benchmark(const std::string &name, T body) {
+    auto before = chrono::steady_clock::now();
+    for (uint64_t i = 0; i < N_ITERATIONS; ++i) {
+        body();
+    }
+    auto after = chrono::steady_clock::now();
+    auto ns = chrono::duration_cast<chrono::nanoseconds>(after - before);
+    cout << name << ": " << (ns/N_ITERATIONS).count() << "ns/op" << "\n";
+}
+
+void bench_rotate() {
+    Cube cube;
+    benchmark("rotate", [cube]() {
+            cube.apply(cube);
+        });
+}
+
+void bench_invert() {
+    Cube cube;
+    benchmark("invert", [cube]() {
+            cube.invert();
+        });
+}
+
+int main() {
+    bench_rotate();
+    bench_invert();
+    return 0;
+}
