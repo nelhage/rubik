642a28cedd08efc4f6818da1197ad8ea05b12a02
Nelson Elhage <nelhage@nelhage.com>
2018-11-10 17:06:01 +0000

    produce both matrices

--- scripts/allpairs.py
+++ scripts/allpairs.py
@@ -12,21 +12,30 @@ moves = [
 
 moves += [m.invert() for m in moves]
 
-def coord(align, perm):
-    return 12*align+perm
+def adjacency(moves, nperm, nalign):
+    def coord(a, p):
+        return a*nperm+p
+
+    dim = nperm*nalign
+    adj = np.ndarray((dim, dim), dtype=np.uint32)
+    adj.fill(1000)
+
+    for (perm, align) in moves:
+        for i,j in enumerate(perm):
+            for n in range(nalign):
+                adj[coord(n, i)][coord((n+align[i])%nalign, j)] = 1
+
+    for k in range(dim):
+        adj = np.minimum(adj, adj[np.newaxis,k,:] + adj[:,k,np.newaxis])
+    return adj
 
 
-adj = np.ndarray((24, 24))
-adj.fill(np.inf)
+print("# edges")
+print(adjacency(
+    [(m.edge_perm, m.edge_align) for m in moves],
+    12, 2))
 
-for m in moves:
-    for i,j in enumerate(m.edge_perm):
-        adj[coord(0, i)][coord(m.edge_align[i], j)] = 1
-        adj[coord(1, i)][coord(not m.edge_align[i], j)] = 1
-
-print(adj)
-
-for k in range(24):
-    adj = np.minimum(adj, adj[np.newaxis,k,:] + adj[:,k,np.newaxis])
-
-print(adj)
+print("# corners")
+print(adjacency(
+    [(m.corner_perm, m.corner_align) for m in moves],
+    8, 3))
