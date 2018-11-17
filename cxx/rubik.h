de525ca2e1d5383142696a9ad219c8b434b44396
Nelson Elhage <nelhage@nelhage.com>
2018-11-17 03:55:37 +0000

    publicize these

--- cxx/rubik.h
+++ cxx/rubik.h
@@ -30,13 +30,6 @@ enum class Face : char {
 class Cube {
     friend class Rotations;
 
-    static constexpr uint8_t kEdgePermMask    = 0x0f;
-    static constexpr uint8_t kEdgeAlignMask   = 0x10;
-    static constexpr uint8_t kEdgeAlignShift  = 4;
-    static constexpr uint8_t kCornerPermMask  = 0x07;
-    static constexpr uint8_t kCornerAlignMask = 0x30;
-    static constexpr uint8_t kCornerAlignShift  = 4;
-
     __m128i edges;
     __m128i corners;
 
@@ -44,6 +37,13 @@ class Cube {
     Cube(std::array<uint8_t, 12> edges,
          std::array<uint8_t, 8> corners);
 public:
+    static constexpr uint8_t kEdgePermMask    = 0x0f;
+    static constexpr uint8_t kEdgeAlignMask   = 0x10;
+    static constexpr uint8_t kEdgeAlignShift  = 4;
+    static constexpr uint8_t kCornerPermMask  = 0x07;
+    static constexpr uint8_t kCornerAlignMask = 0x30;
+    static constexpr uint8_t kCornerAlignShift  = 4;
+
     Cube();
     Cube apply(const Cube &rhs) const;
     Cube invert() const;
